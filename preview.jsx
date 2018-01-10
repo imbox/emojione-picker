c3697e68fc6ab5e8196bacd575c83da55ea78e4c
richie-south <richard.soderman@gmail.com>
2018-01-10 17:42:20 +0100

    fix: removed old dependency

--- preview.jsx
+++ preview.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactPerf from 'react-addons-perf';
 import EmojiPicker from './src/picker';
-
-// Expose React Perf on the window for the Chrome React Perf extension
-window.Perf = ReactPerf;
 
 const logChoice = function(emoji) {
   console.log(emoji);
